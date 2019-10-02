e9194747cd4343477fa288b1cb898d5a99fd2a0c
singuyen123 <nguyentrungsi6997@gmail.com>
2019-10-02 12:29:52 +0700

    send msg from kernel to server

--- src/mqtt_client/mqtt_client.c
+++ src/mqtt_client/mqtt_client.c
@@ -2,261 +2,240 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
-#include <netdb.h> 
-#include <netinet/in.h> 
+#include <netdb.h>
+#include <netinet/in.h>
 #include <linux/netlink.h>
 #include <pthread.h>
+#include "syslog.h"
 #include "MQTTClient.h"
 
 /* define for mqtt_client */
-#define ADDRESS     "test.mosquitto.org"
-#define CLIENTID    "ExampleClientSub"
-#define TOPIC       "presence"
-#define PAYLOAD     "Hello World!"
-#define QOS         1
-#define TIMEOUT     10000L
+#define ADDRESS "test.mosquitto.org"
+#define CLIENTID "ExampleClientSub"
+#define TOPIC "presence"
+#define PAYLOAD "Hello World!"
+#define QOS 1
+#define TIMEOUT 10000L
 /* define for socket TCP*/
-#define MAX 80 
+#define MAX 80
 #define PORT_TCP 6997
-#define SA struct sockaddr 
+#define SA struct sockaddr
 /* define for netlink*/
 #define NETLINK_USER 31
 #define NETLINK_USER1 21
 #define MAX_PAYLOAD 1024 /* maximum payload size*/
 
+MQTTClient client;
+MQTTClient_connectOptions conn_opts = MQTTClient_connectOptions_initializer;
+MQTTClient_message pubmsg = MQTTClient_message_initializer;
+MQTTClient_deliveryToken token;
+int rc;
+int ch;
+
 /* code netlink*/
-void *netlink_rev(void *netlink_user)
-{
-    struct sockaddr_nl src_addr, dest_addr;
-    struct nlmsghdr *nlh = NULL;
-    struct iovec iov;
-    int sock_fd;
-    struct msghdr msg;
-    int nl = atoi(netlink_user);
-    sock_fd = socket(PF_NETLINK, SOCK_RAW, nl);
-    if (sock_fd < 0)
-        return -1;
+void *netlink_rev(void *netlink_user) {
+  struct sockaddr_nl src_addr, dest_addr;
+  struct nlmsghdr *nlh = NULL;
+  struct iovec iov;
+  int sock_fd;
+  struct msghdr msg;
+  int nl = atoi(netlink_user);
+  sock_fd = socket(PF_NETLINK, SOCK_RAW, nl);
+  if (sock_fd < 0)
+    return -1;
 
-    memset(&src_addr, 0, sizeof(src_addr));
-    src_addr.nl_family = AF_NETLINK;
-    src_addr.nl_pid = getpid(); /* self pid */
+  memset(&src_addr, 0, sizeof(src_addr));
+  src_addr.nl_family = AF_NETLINK;
+  src_addr.nl_pid = getpid(); /* self pid */
 
-    bind(sock_fd, (struct sockaddr *)&src_addr, sizeof(src_addr));
+  bind(sock_fd, (struct sockaddr *)&src_addr, sizeof(src_addr));
 
-    memset(&dest_addr, 0, sizeof(dest_addr));
-    memset(&dest_addr, 0, sizeof(dest_addr));
-    dest_addr.nl_family = AF_NETLINK;
-    dest_addr.nl_pid = 0;    /* For Linux Kernel */
-    dest_addr.nl_groups = 0; /* unicast */
+  memset(&dest_addr, 0, sizeof(dest_addr));
+  memset(&dest_addr, 0, sizeof(dest_addr));
+  dest_addr.nl_family = AF_NETLINK;
+  dest_addr.nl_pid = 0;    /* For Linux Kernel */
+  dest_addr.nl_groups = 0; /* unicast */
 
-    nlh = (struct nlmsghdr *)malloc(NLMSG_SPACE(MAX_PAYLOAD));
-    memset(nlh, 0, NLMSG_SPACE(MAX_PAYLOAD));
-    nlh->nlmsg_len = NLMSG_SPACE(MAX_PAYLOAD);
-    nlh->nlmsg_pid = getpid();
-    nlh->nlmsg_flags = 0;
+  nlh = (struct nlmsghdr *)malloc(NLMSG_SPACE(MAX_PAYLOAD));
+  memset(nlh, 0, NLMSG_SPACE(MAX_PAYLOAD));
+  nlh->nlmsg_len = NLMSG_SPACE(MAX_PAYLOAD);
+  nlh->nlmsg_pid = getpid();
+  nlh->nlmsg_flags = 0;
 
-    strcpy(NLMSG_DATA(nlh), "Hello");
+  strcpy(NLMSG_DATA(nlh), "Hello");
 
-    iov.iov_base = (void *)nlh;
-    iov.iov_len = nlh->nlmsg_len;
-    msg.msg_name = (void *)&dest_addr;
-    msg.msg_namelen = sizeof(dest_addr);
-    msg.msg_iov = &iov;
-    msg.msg_iovlen = 1;
+  iov.iov_base = (void *)nlh;
+  iov.iov_len = nlh->nlmsg_len;
+  msg.msg_name = (void *)&dest_addr;
+  msg.msg_namelen = sizeof(dest_addr);
+  msg.msg_iov = &iov;
+  msg.msg_iovlen = 1;
 
-    printf("Sending message to kernel\n");
-    sendmsg(sock_fd, &msg, 0);
-    printf("Waiting for message from kernel\n");
+  syslog(LOG_INFO, "Sending message to kernel\n");
+  sendmsg(sock_fd, &msg, 0);
+  syslog(LOG_INFO, "Waiting for message from kernel\n");
 
-    /* Read message from kernel */
-    while (1)
-    {
-        recvmsg(sock_fd, &msg, 0);
-        printf("Received message payload: %s\n", (char *)NLMSG_DATA(nlh));
-    }
+  /* Read message from kernel */
+  while (1) {
+    recvmsg(sock_fd, &msg, 0);
+    printf("Received message payload: %s\n", (char *)NLMSG_DATA(nlh));
+  }
 
-    close(sock_fd);
+  close(sock_fd);
 }
 /* end code netlink*/
 
 /* code socket TCP*/
-void func(int sockfd)
-{
-    char buff[MAX];
-    int n;
-    // infinite loop for chat
-    for (;;)
-    {
-        bzero(buff, MAX);
+void func(int sockfd) {
+  char buff[MAX];
+  int n;
+  // infinite loop for chat
+  for (;;) {
+    bzero(buff, MAX);
 
-        // read the message from client and copy it in buffer
-        read(sockfd, buff, sizeof(buff));
-        // print buffer which contains the client contents
-        printf("From client: %s\t To client : ", buff);
-        bzero(buff, MAX);
-        n = 0;
-        // copy server message in the buffer
-        while ((buff[n++] = getchar()) != '\n')
-            ;
-
-        // and send that buffer to client
-        write(sockfd, buff, sizeof(buff));
-
-        // if msg contains "Exit" then server exit and chat ended.
-        if (strncmp("exit", buff, 4) == 0)
-        {
-            printf("Server Exit...\n");
-            break;
-        }
+    // read the message from client and copy it in buffer
+    read(sockfd, buff, sizeof(buff));
+    // print buffer which contains the client contents
+    if (strncmp(buff, "usb", 3) == 0) {
+      subscribe_topic("USB", "usb detected");
     }
+    n = 0;
+  }
 }
 
-void *run_socket_tcp()
-{
-    int sockfd, connfd, len;
-    struct sockaddr_in servaddr, cli;
+void *run_socket_tcp() {
+  int sockfd, connfd, len;
+  struct sockaddr_in servaddr, cli;
 
-    // socket create and verification
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd == -1)
-    {
-        printf("socket creation failed...\n");
-        exit(0);
-    }
-    else
-        printf("Socket successfully created..\n");
-    bzero(&servaddr, sizeof(servaddr));
+  // socket create and verification
+  sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  if (sockfd == -1) {
+    syslog(LOG_ERR, "socket creation failed...\n");
+    exit(0);
+  }
+  else
+    syslog(LOG_INFO, "Socket successfully created..\n");
+  bzero(&servaddr, sizeof(servaddr));
 
-    // assign IP, PORT
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    servaddr.sin_port = htons(PORT_TCP);
+  // assign IP, PORT
+  servaddr.sin_family = AF_INET;
+  servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+  servaddr.sin_port = htons(PORT_TCP);
 
-    // Binding newly created socket to given IP and verification
-    if ((bind(sockfd, (SA *)&servaddr, sizeof(servaddr))) != 0)
-    {
-        printf("socket bind failed...\n");
-        exit(0);
-    }
-    else
-        printf("Socket successfully binded..\n");
+  // Binding newly created socket to given IP and verification
+  if ((bind(sockfd, (SA *)&servaddr, sizeof(servaddr))) != 0) {
+    syslog(LOG_ERR, "socket bind failed...\n");
+    exit(0);
+  }
+  else
+    syslog(LOG_INFO, "Socket successfully binded..\n");
 
-    // Now server is ready to listen and verification
-    if ((listen(sockfd, 5)) != 0)
-    {
-        printf("Listen failed...\n");
-        exit(0);
-    }
-    else
-        printf("Server listening..\n");
-    len = sizeof(cli);
+  // Now server is ready to listen and verification
+  if ((listen(sockfd, 5)) != 0) {
+    syslog(LOG_ERR, "Listen failed...\n");
+    exit(0);
+  }
+  else
+    syslog(LOG_INFO, "Server listening..\n");
+  len = sizeof(cli);
 
-    // Accept the data packet from client and verification
-    connfd = accept(sockfd, (SA *)&cli, &len);
-    if (connfd < 0)
-    {
-        printf("server acccept failed...\n");
-        exit(0);
-    }
-    else
-        printf("server acccept the client...\n");
+  // Accept the data packet from client and verification
+  connfd = accept(sockfd, (SA *)&cli, &len);
+  if (connfd < 0) {
+    syslog(LOG_ERR, "server acccept failed...\n");
+    exit(0);
+  }
+  else
+    syslog(LOG_INFO, "server acccept the client...\n");
 
-    // Function for chatting between client and server
-    func(connfd);
+  // Function for chatting between client and server
+  func(connfd);
 
-    // After chatting close the socket
-    close(sockfd);
+  // After chatting close the socket
+  close(sockfd);
 }
 /* end code socket TCP */
 
-
 volatile MQTTClient_deliveryToken deliveredtoken;
 
-void delivered(void *context, MQTTClient_deliveryToken dt)
-{
-    printf("Message with token value %d delivery confirmed\n", dt);
-    deliveredtoken = dt;
+void delivered(void *context, MQTTClient_deliveryToken dt) {
+  syslog(LOG_INFO, "Message with token value %d delivery confirmed\n", dt);
+  deliveredtoken = dt;
 }
 
-int msgarrvd(void *context, char *topicName, int topicLen, MQTTClient_message *message)
-{
-    int i;
-    char* payloadptr;
+int msgarrvd(void *context, char *topicName, int topicLen, MQTTClient_message *message) {
+  int i;
+  char *payloadptr;
 
-    printf("Message arrived\n");
-    printf("     topic: %s\n", topicName);
-    printf("   message: ");
+  syslog(LOG_INFO, "Message arrived\n");
+  syslog(LOG_INFO, "     topic: %s\n", topicName);
+  syslog(LOG_INFO, "   message: ");
 
-    payloadptr = message->payload;
-    for(i=0; i<message->payloadlen; i++)
-    {
-        putchar(*payloadptr++);
-    }
-    putchar('\n');
-    MQTTClient_freeMessage(&message);
-    MQTTClient_free(topicName);
-    return 1;
+  payloadptr = message->payload;
+  for (i = 0; i < message->payloadlen; i++) {
+    putchar(*payloadptr++);
+  }
+  putchar('\n');
+  MQTTClient_freeMessage(&message);
+  MQTTClient_free(topicName);
+  return 1;
 }
 
-void connlost(void *context, char *cause)
-{
-    printf("\nConnection lost\n");
-    printf("     cause: %s\n", cause);
+void connlost(void *context, char *cause) {
+  syslog(LOG_INFO, "\nConnection lost\n");
+  syslog(LOG_INFO, "     cause: %s\n", cause);
 }
 
-int main(int argc, char* argv[])
-{
-    pthread_t thread_netlink1, thread_netlink2, thread_TCP;
-    pthread_create(&thread_netlink1, NULL, netlink_rev, "31");
-    pthread_create(&thread_netlink2, NULL, netlink_rev, "21");
-    pthread_create(&thread_TCP, NULL, run_socket_tcp, NULL);
+void subscribe_topic(char *topic, char *msg) {
+  MQTTClient_subscribe(client, topic, QOS);
 
-    MQTTClient client;
-    MQTTClient_connectOptions conn_opts = MQTTClient_connectOptions_initializer;
-    MQTTClient_message pubmsg = MQTTClient_message_initializer;
-    MQTTClient_deliveryToken token;
-    int rc;
-    int ch;
+  pubmsg.payload = msg;
+  pubmsg.payloadlen = (int)strlen(msg);
+  pubmsg.qos = QOS;
+  pubmsg.retained = 0;
+  MQTTClient_publishMessage(client, topic, &pubmsg, &token);
+  syslog(LOG_INFO, "Waiting for up to %d seconds for publication of %s\n"
+                   "on topic %s for client with ClientID: %s\n",
+         (int)(TIMEOUT / 1000), PAYLOAD, topic, CLIENTID);
+  rc = MQTTClient_waitForCompletion(client, token, TIMEOUT);
+  syslog(LOG_INFO, "Message with delivery token %d delivered\n", token);
 
-    MQTTClient_create(&client, ADDRESS, CLIENTID,
-        MQTTCLIENT_PERSISTENCE_NONE, NULL);
-    conn_opts.keepAliveInterval = 20;
-    conn_opts.cleansession = 1;
+  do {
+    ch = getchar();
+  } while (ch != 'Q' && ch != 'q');
 
-    MQTTClient_setCallbacks(client, NULL, connlost, msgarrvd, delivered);
+  MQTTClient_unsubscribe(client, topic);
+}
 
-    if ((rc = MQTTClient_connect(client, &conn_opts)) != MQTTCLIENT_SUCCESS)
-    {
-        printf("Failed to connect, return code %d\n", rc);
-        exit(EXIT_FAILURE);
-    }
-    printf("Subscribing to topic %s\nfor client %s using QoS%d\n\n"
-           "Press Q<Enter> to quit\n\n", TOPIC, CLIENTID, QOS);
-    MQTTClient_subscribe(client, TOPIC, QOS);
+int main(int argc, char *argv[]) {
+  pthread_t thread_netlink1, thread_netlink2, thread_TCP;
 
-    pubmsg.payload = PAYLOAD;
-    pubmsg.payloadlen = (int)strlen(PAYLOAD);
-    pubmsg.qos = QOS;
-    pubmsg.retained = 0;
-    MQTTClient_publishMessage(client, TOPIC, &pubmsg, &token);
-    printf("Waiting for up to %d seconds for publication of %s\n"
-            "on topic %s for client with ClientID: %s\n",
-            (int)(TIMEOUT/1000), PAYLOAD, TOPIC, CLIENTID);
-    rc = MQTTClient_waitForCompletion(client, token, TIMEOUT);
-    printf("Message with delivery token %d delivered\n", token);
+  MQTTClient_create(&client, ADDRESS, CLIENTID,
+                    MQTTCLIENT_PERSISTENCE_NONE, NULL);
+  conn_opts.keepAliveInterval = 20;
+  conn_opts.cleansession = 1;
 
-    do 
-    {
-        ch = getchar();
-    } while(ch!='Q' && ch != 'q');
+  MQTTClient_setCallbacks(client, NULL, connlost, msgarrvd, delivered);
 
-    MQTTClient_unsubscribe(client, TOPIC);
-    MQTTClient_disconnect(client, 10000);
-    MQTTClient_destroy(&client);
+  if ((rc = MQTTClient_connect(client, &conn_opts)) != MQTTCLIENT_SUCCESS) {
+    syslog(LOG_ERR, "Failed to connect, return code %d\n", rc);
+    exit(EXIT_FAILURE);
+  }
+  syslog(LOG_INFO, "Subscribing to topic %s\nfor client %s using QoS%d\n\n"
+                   "Press Q<Enter> to quit\n\n",
+         TOPIC, CLIENTID, QOS);
+  pthread_create(&thread_netlink1, NULL, netlink_rev, "31");
+  pthread_create(&thread_netlink2, NULL, netlink_rev, "21");
+  pthread_create(&thread_TCP, NULL, run_socket_tcp, NULL);
 
-    pthread_join(thread_netlink1, NULL);
-    pthread_join(thread_netlink2, NULL);
-    pthread_join(thread_TCP, NULL);
-    while (1);
-    return rc;
+  pthread_join(thread_netlink1, NULL);
+  pthread_join(thread_netlink2, NULL);
+  run_socket_tcp();
+
+  MQTTClient_disconnect(client, 10000);
+  MQTTClient_destroy(&client);
+
+  while (1)
+    ;
+  return rc;
 }
--- src/service_user_space/client.c
+++ src/service_user_space/client.c
@@ -1,65 +1,78 @@
 
-// Write CPP code here 
-#include <netdb.h> 
-#include <stdio.h> 
-#include <stdlib.h> 
-#include <string.h> 
-#include <sys/socket.h> 
-#define MAX 80 
-#define PORT 8080 
-#define SA struct sockaddr 
-void func(int sockfd) 
-{ 
-    char buff[MAX]; 
-    int n; 
-    for (;;) { 
-        bzero(buff, sizeof(buff)); 
-        printf("Enter the string : "); 
-        n = 0; 
-        while ((buff[n++] = getchar()) != '\n') 
-            ; 
-        write(sockfd, buff, sizeof(buff)); 
-        bzero(buff, sizeof(buff)); 
-        read(sockfd, buff, sizeof(buff)); 
-        printf("From Server : %s", buff); 
-        if ((strncmp(buff, "exit", 4)) == 0) { 
-            printf("Client Exit...\n"); 
-            break; 
-        } 
-    } 
-} 
-  
-int main() 
-{ 
-    int sockfd, connfd; 
-    struct sockaddr_in servaddr, cli; 
-  
-    // socket create and varification 
-    sockfd = socket(AF_INET, SOCK_STREAM, 0); 
-    if (sockfd == -1) { 
-        printf("socket creation failed...\n"); 
-        exit(0); 
-    } 
-    else
-        printf("Socket successfully created..\n"); 
-    bzero(&servaddr, sizeof(servaddr)); 
-  
-    // assign IP, PORT 
-    servaddr.sin_family = AF_INET; 
-    servaddr.sin_addr.s_addr = inet_addr("127.0.0.1"); 
-    servaddr.sin_port = htons(PORT); 
-  
-    // connect the client socket to server socket 
-    if (connect(sockfd, (SA*)&servaddr, sizeof(servaddr)) != 0) { 
-        printf("connection with the server failed...\n"); 
-        exit(0); 
-    } 
-    else
-        printf("connected to the server..\n"); 
-  
-    // function for chat 
-    func(sockfd); 
-  
-    // close the socket 
-    close(sockfd); 
-} 
+// Write CPP code here
+#include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <sys/socket.h>
+#include <syslog.h>
+
+#define MAX 80
+#define PORT 6997
+#define SA struct sockaddr
+
+void delay(int number_of_seconds) {
+  // Converting time into milli_seconds
+  int milli_seconds = 1000 * number_of_seconds;
+
+  // Stroing start time
+  clock_t start_time = clock();
+
+  // looping till required time is not acheived
+  while (clock() < start_time + milli_seconds)
+    ;
+}
+
+int main() {
+  int sockfd, connfd;
+  struct sockaddr_in servaddr, cli;
+
+  // socket create and varification
+  sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  if (sockfd == -1) {
+    syslog(LOG_ERR, "socket creation failed...\n");
+    exit(0);
+  }
+  else
+    syslog(LOG_INFO, "Socket successfully created..\n");
+  bzero(&servaddr, sizeof(servaddr));
+
+  // assign IP, PORT
+  servaddr.sin_family = AF_INET;
+  servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+  servaddr.sin_port = htons(PORT);
+
+  // connect the client socket to server socket
+  if (connect(sockfd, (SA *)&servaddr, sizeof(servaddr)) != 0) {
+    syslog(LOG_INFO, "connection with the server failed...\n");
+    exit(0);
+  }
+  else
+    syslog(LOG_INFO, "connected to the server..\n");
+
+  // function for chat
+  while (1) {
+    FILE *f = popen("mount | grep /dev/sdb1", "r");
+    if (NULL != f)
+    {
+      /* test if something has been outputed by 
+           the command */
+      if (EOF == fgetc(f))
+      {
+        puts("/dev/sdb1 is NOT mounted");
+      }
+      else
+      {
+        puts("/dev/sdb1 is mounted");
+        write(sockfd, "usb", sizeof("usb"));
+      }
+      /* close the command file */
+      pclose(f);
+    }
+    delay(1000);
+  }
+
+  // close the socket
+  close(sockfd);
+}
